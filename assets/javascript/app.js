9eb88ceeeec41a7c18e8614239a350702ff6250a
Your Name <you@example.com>
2017-10-18 11:53:50 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,60 +19,54 @@ var questions = [{
 	    question: "Who was 'Micheal Myers' first victim at age 6??",
 	    choices: ["His Mother", "His Sister","His Teacher", "The Town Sheriff"],
 	    answer: 1,
-	    video: "",
-	    time: 1000*30
 	},{
 		question: "Ghostface's mask in Scream is based on a painting by which famous artist?",
 		choices: ["Edouard Manet", "Andy Warhol", "Edvard Munch", "Claude Monet"],
 		answer: 2,
-		video: "",
-		time: 1000*30
 	},{
 		question: "What was used for blood in the movie 'Psycho'?",
 		choices: ["Catups", "Corn syrup and Food coloring"," Chocolate Syrup"," Real Blood"],
 		answer: 2,
 		video: "",
-		time: 1000*30
 	},{
 		question: "In the movie 'The Omen', who hangs themselves at Damien's fifth birthday party?",
 		choices: ["His brother"," His Nanny"," His Priest", "His Mother"],
 		answer: 1,
-		video: "",
-		time: 1000*30
 	},{
 		question: "In the movie 'saw', what part of the body did the character had to imputate to escape in the end?",
 		choices: ["His Left Hand", "His Left foot"," His Right Hand"," His Right foot"],
 		answer: 3,
-		video: "",
-		time: 1000*30
 	}
 ];
 var pageCount;
 var qPageTime = 30;
 var timer = 5;
-var i= 0;
+var i = 0;
 var correct= 0
-var incorrect=0
-
+var incorrect= 0
+var intervalId;
 //you have to display correct and incorrect to html 67-61
 //create a function to show result page after the last question
 
 //start button to begin
-$("#start-btn").on("click",function(){
-	$("#start").hide();
-	$("#trivia-questions").show();
-	displayQuestions(i)
-});
+function startGame(){
+	$("#start-btn").on("click",function() {
+		$("#start").hide();
+		$("#trivia-questions").show();
+		displayQuestions(i)
+	});
+}
+startGame()	
 //function to display questions [i] is to run through all of the questions for the global i variable
-function displayQuestions(i){
+function displayQuestions(i) {
 	$("#questions").html(questions[i].question)
 	$("#choice0").html(questions[i].choices[0])
 	$("#choice1").html(questions[i].choices[1])
 	$("#choice2").html(questions[i].choices[2])
 	$("#choice3").html(questions[i].choices[3])
 }
-
-function qTimer(){
+//timer function
+function qTimer() {
 	timer--;
 	$("#timer").html(timer)
 	if (timer === 0){
@@ -80,25 +74,39 @@ function qTimer(){
 		$("#timer").html(timer)
 		if(i >= questions.length){
 			console.log("game over")
+			//show result page when all of the questions are asked
+			$("#start").hide();
+			$("#trivia-questions").hide();
+			$("#questions-results").show();
 		}
 		displayQuestions(i)
 		timer=5
 	}
 }
 //trying to move to the next question
-setInterval(qTimer, 1000)
+//stop time
+function stop(){
+      clearInterval(intervalId);
+ }
 
-function answer(){
+setInterval(qTimer, 1000)
+//logging in right answers
+function answer(i) {
 	$("button").on("click", function() {
+		if(questions ){
+			correct++
+			console.log("you are correct")
+
+		}
 		console.log(this)
-	})
+	});
 }
-answer();
+answer(i);
 //-------------------------------------now grab the result when click on button 
 //result button
 //show result id
-// function nextQ(){
-// 	for ( var i = 0; i <questions.length; i++ ){
+//function nextQ(){
+//for ( var i = 0; i <questions.length; i++ ){
 // 		displayQuestions(i)
 // 	}
 // }
