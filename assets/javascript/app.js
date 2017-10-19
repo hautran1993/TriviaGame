c8862960aca8d115e09c07e555acbe0bb971e9a8
Your Name <you@example.com>
2017-10-19 18:43:21 -0500

    60%

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,103 +15,121 @@ $(document).ready(function(){
 //create var for array of questions
 $("#trivia-questions").hide();
 $("#questions-results").hide();
-var questions = [{
-	    question: "Who was 'Micheal Myers' first victim at age 6??",
-	    choices: ["His Mother", "His Sister","His Teacher", "The Town Sheriff"],
-	    answer: 1,
-	},{
-		question: "Ghostface's mask in Scream is based on a painting by which famous artist?",
-		choices: ["Edouard Manet", "Andy Warhol", "Edvard Munch", "Claude Monet"],
-		answer: 2,
-	},{
-		question: "What was used for blood in the movie 'Psycho'?",
-		choices: ["Catups", "Corn syrup and Food coloring"," Chocolate Syrup"," Real Blood"],
-		answer: 2,
-		video: "",
-	},{
-		question: "In the movie 'The Omen', who hangs themselves at Damien's fifth birthday party?",
-		choices: ["His brother"," His Nanny"," His Priest", "His Mother"],
-		answer: 1,
-	},{
-		question: "In the movie 'saw', what part of the body did the character had to imputate to escape in the end?",
-		choices: ["His Left Hand", "His Left foot"," His Right Hand"," His Right foot"],
-		answer: 3,
-	}
-];
-var pageCount;
-var qPageTime = 30;
-var timer = 5;
-var i = 0;
-var correct= 0
-var incorrect= 0
-var intervalId;
+	var questions = [{
+		    question: "Who was 'Micheal Myers' first victim at age 6??",
+		    choices: ["His Mother", "His Sister","His Teacher", "The Town Sheriff"],
+		    answer: 1,
+		},{
+			question: "Ghostface's mask in Scream is based on a painting by which famous artist?",
+			choices: ["Edouard Manet", "Andy Warhol", "Edvard Munch", "Claude Monet"],
+			answer: 2,
+		},{
+			question: "What was used for blood in the movie 'Psycho'?",
+			choices: ["Catups", "Corn syrup and Food coloring"," Chocolate Syrup"," Real Blood"],
+			answer: 2,
+	
+		},{
+			question: "In the movie 'The Omen', who hangs themselves at Damien's fifth birthday party?",
+			choices: ["His brother"," His Nanny"," His Priest", "His Mother"],
+			answer: 1,
+		},{
+			question: "In the movie 'saw', what part of the body did the character had to imputate to escape in the end?",
+			choices: ["His Left Hand", "His Left foot"," His Right Hand"," His Right foot"],
+			answer: 3,
+		}
+	];
+	var pageCount;
+	var timer = 5;
+	var i = 0;
+	var correct= 0
+	var incorrect= 0
+	var intervalId;
 //you have to display correct and incorrect to html 67-61
 //create a function to show result page after the last question
 
 //start button to begin
-function startGame(){
-	$("#start-btn").on("click",function() {
-		$("#start").hide();
-		$("#trivia-questions").show();
-		displayQuestions(i)
-	});
-}
-startGame()	
-//function to display questions [i] is to run through all of the questions for the global i variable
-function displayQuestions(i) {
-	$("#questions").html(questions[i].question)
-	$("#choice0").html(questions[i].choices[0])
-	$("#choice1").html(questions[i].choices[1])
-	$("#choice2").html(questions[i].choices[2])
-	$("#choice3").html(questions[i].choices[3])
-}
-//timer function
-function qTimer() {
-	timer--;
-	$("#timer").html(timer)
-	if (timer === 0){
-		i++;
-		$("#timer").html(timer)
-		if(i >= questions.length){
-			console.log("game over")
-			//show result page when all of the questions are asked
+	function startGame(){
+		$("#start-btn").on("click",function() {
 			$("#start").hide();
-			$("#trivia-questions").hide();
-			$("#questions-results").show();
-		}
-		displayQuestions(i)
-		timer=5
+			$("#trivia-questions").show();
+			displayQuestions();
+			intervalId = setInterval(qTimer, 1000);
+		});
 	}
-}
-//trying to move to the next question
-//stop time
-function stop(){
-      clearInterval(intervalId);
- }
 
-setInterval(qTimer, 1000)
-//logging in right answers
-function answer(i) {
-	$("button").on("click", function() {
-		if(questions ){
-			correct++
-			console.log("you are correct")
+	startGame();
+
+//function to display questions [i] is to run through all of the questions for the global i variable
+	function displayQuestions() {
+		$("#questions").html(questions[i].question);
+		$(".answers").empty()
+		timer=5
+		for ( var j = 0; j < questions[i].choices.length; j++) {
+			var button = $("<button>");
+			button.html(questions[i].choices[j]);
+			button.addClass("answer-data");
+			button.attr("data-answer", questions[i].answer);
+			//should be last because you are adding to the page
+			$(".answers").append(button);
 
 		}
-		console.log(this)
-	});
-}
-answer(i);
-//-------------------------------------now grab the result when click on button 
-//result button
-//show result id
-//function nextQ(){
-//for ( var i = 0; i <questions.length; i++ ){
-// 		displayQuestions(i)
-// 	}
-// }
+	}
+	//next question function
+	function nextQ(){
+		var nButton=$("<button>");
 
- 	
+		i++;
+		displayQuestions()
+		timer=5;
+	}
+
+	$("#next-btn").on("click",function() {
+	nextQ();
+	});
+
+	function grabResult(){
+		$("answer").click(function(){
+
+		});
+		console.log(this)
+	}
+	grabResult()
+
+//timer function
+	function qTimer() {
+		timer--;
+		$("#timer").html(timer)
+		if (timer === 0){
+			i++;
+			$("#timer").html(timer)
+				if(i >= questions.length){
+					console.log("game over")
+					//show result page when all of the questions are asked
+					$("#start").hide();
+					$("#trivia-questions").hide();
+					$("#questions-results").show();
+				}
+			displayQuestions();
+			timer=5;
+		}
+	}
+	function answer(){
+		$("#questions").empty()
+
+	}	
+	//stop time
+	function stop(){
+      clearInterval(intervalId);
+ 	}
+	//-----time interval count down
+
+
+	//-------------------------------------now grab the result when click on button 
+
+	
+//doesn't work
+	
+
 //when "start" button is clicked
 // function startGame(){
 
@@ -148,27 +166,7 @@ answer(i);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
-
-
-
-
-
 
 
 
